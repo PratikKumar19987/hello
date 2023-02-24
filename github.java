b05550aa831a79fdad63a5dd658f127353eeffe0
PratikKumar19987 <78965580+PratikKumar19987@users.noreply.github.com>
2023-02-25 00:41:52 +0530

    Add files via upload
    First Commit

---
+++ github.java
@@ -0,0 +1,14 @@
+package git;
+
+public class github {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello World");
+		System.out.println("Hi");
+		System.out.println("Pratik Kumar");
+		System.out.println("Github");
+		System.out.println("Java and Web Development");
+	}
+
+}
